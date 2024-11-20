4f9e76b4d8d54938b614492dd4fd935eaa2cad3f
tanscentry <1161573915@qq.com>
2024-11-21 02:02:20 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -4085,7 +4085,7 @@ var SignJWT = class extends ProduceJWT {
 };
 
 // src/worker.js
-var userID = "89b3cbba-e6ac-485a-9481-976a0415eab9";
+var userID = "6a0c7428-8671-4f90-84a9-bddf50a8f99d";
 var trojanPassword = `bpb-trojan`;
 var proxyIPs = ["bpb.yousef.isegaro.com"];
 var defaultHttpPorts = ["80", "8080", "2052", "2082", "2086", "2095", "8880"];
